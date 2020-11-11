3d3a2e84f1d56e0330942e93de6240b2e8d6d2cc
Richard <yangrich91@gmail.com>
2020-11-11 04:26:47 -0500

    media queries

--- src/components/About.js
+++ src/components/About.js
@@ -10,16 +10,16 @@ const About = () => {
           About me
         </div>
         <div className="contentContainer">
-          <div>
+          <div className="pictureContainer">
             <img src={ProfilePic} className="profilePic" alt="profile"></img>
           </div>
           <p className="aboutMe">
-            I am a developer that just recently finished a 12-week intensive
-            full stack bootcamp at General Assembly. I am looking for
-            opportunities to further my knowledge while also contributing to
-            something bigger. I strive to push my boundaries every day and aim
-            to learn something new with every project I take on. I am
-            continuously learning and pushing myself to greater heights.{" "}
+            I am a software engineer with a background in the sales and
+            logistics industry. I have always had a passion for solving
+            problems, puzzles, games and I bring that same enthusiasm to any
+            task at hand. My past leadership roles and experience in working
+            together with others ensure I am always in a position to get the job
+            done.
           </p>
         </div>
       </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "../styles/contact.css";
+import GitHub from "../images/github.png";
+import LinkedIn from "../images/linkedIn.png";
+import PDF from "../images/pdf.png";
 
 const Contact = () => {
   return (
@@ -8,10 +11,29 @@ const Contact = () => {
         <div className="title" id="contacts">
           Let's Talk
         </div>
+        <div className="profLinks">
+          <div className="icons">
+            <a href="https://github.com/yang-rich?tab=repositories">
+              <img src={GitHub} className="gitHub" alt="gitHub"></img>
+            </a>
+          </div>
+          <div className="icons">
+            <a href="https://www.linkedin.com/in/yang-rich/">
+              <img src={LinkedIn} className="linkedIn" alt="LinkedIn"></img>
+            </a>
+          </div>
+          <div className="icons">
+            <a href="https://drive.google.com/file/d/1Xj1FhbKIkLvmW1UaruU-Gw8hwSXm7MtC/view?usp=sharing">
+              <img src={PDF} className="pdf" alt="Resume"></img>
+            </a>
+          </div>
+        </div>
         <div className="contactMe">Contact me:</div>
+        <br />
         <div className="email">
           <a href="mailto: yangrich91@gmail.com">yangrich91@gmail.com</a>
         </div>
+        <br />
       </div>
     </>
   );
